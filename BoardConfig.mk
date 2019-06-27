eb46159e701c09cc28e85765b5f88112c8232c51
JarlPenguin <jarlpenguin@outlook.com>
2019-06-27 15:45:40 +0300

    cereus: LineageOS 16 bringup
    * Nuke board info and bring back prebuilt kernel
    
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,10 +4,11 @@
 DEVICE_PATH := device/xiaomi/cereus
 
 # Board
-TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+#TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
 # Kernel
-TARGET_KERNEL_CONFIG := cereus_defconfig
+# TARGET_KERNEL_CONFIG := cereus_defconfig
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 
 # OTA
 TARGET_OTA_ASSERT_DEVICE := cereus
---
+++ lineage_cereus.mk
@@ -0,0 +1,20 @@
+# Inherit from device
+$(call inherit-product, device/xiaomi/cereus/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := cereus
+PRODUCT_NAME := lineage_cereus
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Redmi 6
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Override fingerprint from stock
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="cereus-user 9 PPR1.180610.011 9.5.23 release-keys" \
+    PRODUCT_NAME="cereus" \
+    TARGET_DEVICE="cereus"
+
+BUILD_FINGERPRINT := xiaomi/cereus/cereus:9/PPR1.180610.011/9.5.23:user/release-keys
