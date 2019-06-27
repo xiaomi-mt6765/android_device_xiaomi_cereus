67557df5a83eec748d53ae87536a17bfd540ce89
JarlPenguin <jarlpenguin@outlook.com>
2019-06-27 15:54:35 +0300

    cereus: nuke fp prop
    * Also copy fingerprint permission xml
    * Already set in /vendor/build.prop
    
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- device.mk
+++ device.mk
@@ -1,9 +1,9 @@
 # Inherit common device config
 $(call inherit-product, device/xiaomi/mt6765-common/mt6765.mk)
 
-# Props
-include $(LOCAL_PATH)/system_prop.mk
-
 # Fingerprint
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
+
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1
