2450b9c1919feb52bd73ba62360ef213990bd36c
JarlPenguin <jarlpenguin@gmail.com>
2019-10-24 16:02:20 +0000

    cereus: inherit vendor makefile
    Signed-off-by: JarlPenguin <jarlpenguin@gmail.com>

--- device.mk
+++ device.mk
@@ -20,3 +20,6 @@ $(call inherit-product, device/xiaomi/mt6765-common/mt6765.mk)
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
+
+# Inherit proprietary parts
+$(call inherit-product-if-exists, vendor/xiaomi/cereus/cereus-vendor.mk)
